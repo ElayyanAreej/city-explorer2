3ecbe50ec6993484797cde7d0b2b8c04b27b1d1b
ElayyanAreej <areej.hossein@gmail.com>
2021-08-18 15:19:11 +0300

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
-import Weather from './Component/Weather';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+// import Weather from './Component/Weather';
 
 // import Form from 'react-bootstrap/Form'
 // import {Button} from 'react-bootstrap';
@@ -66,20 +68,53 @@ class App extends React.Component {
 
   render() {
     return (
-      <>
-        <h1>City Explorer</h1>
-        {/* <input type="text"  placeholder="City Name"/> */}
-        {/* <button onSubmit={this.getLocation}>Get The Map</button> */}
-        <form onSubmit={this.getLocation}>
-          <label>City Name</label>
-          <input type="text" placeholder="Enter City Name" name='city' />
+//       <>
+//         <h1>City Explorer</h1>
+//         {/* <input type="text"  placeholder="City Name"/> */}
+//         {/* <button onSubmit={this.getLocation}>Get The Map</button> */}
+//         <form onSubmit={this.getLocation}>
+//           <label>City Name</label>
+//           <input type="text" placeholder="Enter City Name" name='city' />
 
 
-          <button> Get The Map </button>
+//           <button> Get The Map </button>
 
-        </form>
+//         </form>
 
-        {this.state.showData &&
+//         {this.state.showData &&
+//           <p>{this.state.citySearch} Lat:{this.state.cityData.lat} /Lon:{this.state.cityData.lon} </p>}
+
+//         {this.state.showData &&
+//          <img src={`${this.state.cityImg}`} width={400}  />
+//         }
+
+// {this.state.showData && 
+//   this.state.weatherData.map((item)=>{ 
+//    return  (<div>
+//      {item.date}
+//     {item.description}
+//     </div>)
+//   })
+//    }
+
+//       </> 
+
+<>
+      <h1>City Explorer</h1>
+
+<Form onSubmit={this.getLocation}>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>City Name</Form.Label>
+    <Form.Control type="text" placeholder="Enter City Name" name='city' />
+   
+  </Form.Group>
+
+  <Button variant="primary" type="submit">
+  Explorer!
+  </Button>
+</Form>
+
+{this.state.showData &&
           <p>{this.state.citySearch} Lat:{this.state.cityData.lat} /Lon:{this.state.cityData.lon} </p>}
 
         {this.state.showData &&
@@ -95,7 +130,9 @@ class App extends React.Component {
   })
    }
 
-      </> )
+</>
+
+)
 }
 }
  export default App;
